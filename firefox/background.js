a5a86d86e926c4ca74b07a573ff44e6cacaba4e4
Johnson <j3.soon@msa.hinet.net>
2018-05-04 18:12:57 +0800

    Add v1.0 firefox addon

---
+++ firefox/background.js
@@ -0,0 +1,55 @@
+function changeButtonState(tabInfo) {
+  if (tabInfo.status==='complete' && tabInfo.url.startsWith("https://medium.com/")) {
+    browser.browserAction.enable();
+  } else {
+    browser.browserAction.disable();
+  }
+}
+browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tabInfo) {
+  changeButtonState(tabInfo);
+});
+browser.tabs.onActivated.addListener(function(activeInfo) {
+  browser.tabs.get(activeInfo.tabId).then(function(tabInfo) {
+    changeButtonState(tabInfo);});
+});
+browser.runtime.onInstalled.addListener(function(details) {
+  browser.tabs.query({
+    active: true,
+    lastFocusedWindow: true
+  }).then(function(array_of_tabs) {
+    changeButtonState(array_of_tabs[0]);});
+});
+browser.windows.onFocusChanged.addListener(function(windowId) {
+  browser.tabs.query({
+    active: true,
+    lastFocusedWindow: true
+  }).then(function(array_of_tabs) {
+    changeButtonState(array_of_tabs[0]);});
+});
+browser.browserAction.onClicked.addListener(function(tab) {
+  browser.tabs.query({
+    active: true,
+    lastFocusedWindow: true
+  }).then(function(array_of_tabs) {
+    var tab = array_of_tabs[0];
+    var url = tab.url;
+    var id = url.split("-").pop();
+    if (undefined !== id) {
+      var request = new XMLHttpRequest();
+      request.open("GET", "https://medium.com/p/" + id + "/notes", true);
+      request.onload = function() {
+        if (200 === request.status) {
+          var resp = request.responseText;
+          var match = resp.match(/\"webCanonicalUrl\":\"([^"]+)\"/);
+          if (null !== match && null !== match[1]) {
+            var webCanonicalUrl = match[1];
+            browser.tabs.create({"url": webCanonicalUrl});
+          } else {
+            browser.windows.create({"url": url, "incognito": true});
+          }
+        }
+      };
+      request.send();
+    }
+  });
+});
