5435d3b365c1049f29bb3e199e57dba47471818c
Johnson <j3.soon@msa.hinet.net>
2019-06-07 23:57:53 +0800

    Fix for non-medium domain posts

--- firefox/background.js
+++ firefox/background.js
@@ -1,5 +1,6 @@
 function changeButtonState(tabInfo) {
-  if (tabInfo.status==='complete' && tabInfo.url.startsWith("https://medium.com/")) {
+  // We don't check the domain since there are now custom domains and subdomains.
+  if (tabInfo.status==='complete'/* && tabInfo.url.startsWith("https://medium.com/")*/) {
     browser.browserAction.enable();
   } else {
     browser.browserAction.disable();
@@ -33,23 +34,27 @@ browser.browserAction.onClicked.addListener(function(tab) {
   }).then(function(array_of_tabs) {
     var tab = array_of_tabs[0];
     var url = tab.url;
-    var id = url.split("-").pop();
-    if (undefined !== id) {
-      var request = new XMLHttpRequest();
-      request.open("GET", "https://medium.com/p/" + id + "/notes", true);
-      request.onload = function() {
-        if (200 === request.status) {
-          var resp = request.responseText;
-          var match = resp.match(/\"webCanonicalUrl\":\"([^"]+)\"/);
-          if (null !== match && null !== match[1]) {
-            var webCanonicalUrl = match[1];
-            browser.tabs.create({"url": webCanonicalUrl});
-          } else {
-            browser.windows.create({"url": url, "incognito": true});
+    if (!url.startsWith("https://medium.com/")) {
+      browser.windows.create({"url": url, "incognito": true});
+    } else {
+      var id = url.split("-").pop();
+      if (undefined !== id) {
+        var request = new XMLHttpRequest();
+        request.open("GET", "https://medium.com/p/" + id + "/notes", true);
+        request.onload = function() {
+          if (200 === request.status) {
+            var resp = request.responseText;
+            var match = resp.match(/\"webCanonicalUrl\":\"([^"]+)\"/);
+            if (null !== match && null !== match[1]) {
+              var webCanonicalUrl = match[1];
+              browser.tabs.create({"url": webCanonicalUrl});
+            } else {
+              browser.windows.create({"url": url, "incognito": true});
+            }
           }
-        }
-      };
-      request.send();
+        };
+        request.send();
+      }
     }
   });
 });
